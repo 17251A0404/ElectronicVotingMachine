0560a2245170884ac9c92fa22f25d85ee5946846
stepin105108 <69103743+stepin105108@users.noreply.github.com>
2020-11-11 17:32:22 +0530

    Update VotingSystem.h

--- 3_Implementation/inc/VotingSystem.h
+++ 3_Implementation/inc/VotingSystem.h
@@ -1,1 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#ifndef __VOTINGSYSTEM_H__
+#define __VOTINGSYSTEM_H__
+
+#define CANDIDATE_COUNT
+#define CANDIDATE1 "RAHUL GANDHI"
+#define CANDIDATE2 "NARENDRA MODI"
+#define CANDIDATE3 "AMITH SHAH"
+
+void castVote();
+
+void admin();
+
+int login();
+int Wlogin();
+#endif
 
